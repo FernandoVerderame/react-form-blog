c2c5c24e702b3d2720cd0af1b2313856547e248b
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-06-18 17:14:25 +0200

    added new post inputs

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,28 +2,42 @@
 import { useState } from 'react';
 import formStyle from './Form.module.css';
 import { IoMdClose as Delete } from "react-icons/io";
+import PostCard from '../Card/PostCard';
 
 const Form = () => {
 
-    const [titles, setTitles] = useState([]);
-    const [titleLabel, setTitleLabel] = useState('');
+    const defaultPostData = {
+        title: '',
+        image: '',
+        content: '',
+        published: true,
+        tags: ['html', 'css']
+    }
+
+    const [posts, setPosts] = useState([]);
+
+    const [postData, setPostData] = useState(defaultPostData);
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        setTitles(array => ([...array, titleLabel]));
-        setTitleLabel('');
+        setPosts(array => ([...array, postData]));
+        setPostData(defaultPostData);
     }
 
-    const removeTitle = (indexToDelete) => {
-        setTitles(array => array.filter((_, i) => i !== indexToDelete));
+    const removePost = (indexToDelete) => {
+        setPosts(array => array.filter((_, i) => i !== indexToDelete));
+    }
+
+    const changePostData = (key, newValue) => {
+        setPostData(data => ({ ...data, [key]: newValue }));
     }
 
     return (
         <>
             <div className={formStyle.cardForm}>
                 <div className={formStyle.cardTitle}>
-                    <h3>Creo un nuovo Titolo</h3>
+                    <h3>Creo un nuovo Post</h3>
                 </div>
                 <div className={formStyle.cardBody}>
                     <form onSubmit={handleSubmit}>
@@ -33,28 +47,52 @@ const Form = () => {
                                 type="text"
                                 id='title'
                                 name='title'
-                                value={titleLabel}
-                                onChange={e => setTitleLabel(e.target.value)}
+                                value={postData.title}
+                                onChange={(e) => changePostData('title', e.target.value)}
+                            />
+                        </div>
+                        <div className={formStyle.cardInput}>
+                            <label htmlFor="image">URL Immagine</label>
+                            <input
+                                type="url"
+                                id='image'
+                                name='image'
+                                value={postData.image}
+                                onChange={(e) => changePostData('image', e.target.value)}
+                            />
+                        </div>
+                        <div className={formStyle.cardInput}>
+                            <label htmlFor="content">Content</label>
+                            <textarea
+                                id='content'
+                                name='content'
+                                rows='5'
+                                value={postData.content}
+                                onChange={(e) => changePostData('content', e.target.value)}
                             />
                         </div>
                         <div className={formStyle.cardBtn}>
                             <button>Crea</button>
                         </div>
                     </form>
-
-                    <h3>Lista dei Titoli</h3>
-                    <ul>
-                        {titles.map((title, i) => (
-                            <li key={`title${i}`}>
-                                {title}
-                                <button className={formStyle.deleteBtn}>
-                                    <Delete onClick={() => removeTitle(i)} />
-                                </button>
-                            </li>
-                        ))}
-                    </ul>
                 </div>
-            </div >
+            </div>
+
+            <h3>Lista dei nuovi Post</h3>
+            {posts.map(({ title, image, content, published, tags }, i) => (
+                <div key={`post${i}`} className={formStyle.postCard}>
+                    <PostCard
+                        title={title}
+                        image={image}
+                        content={content}
+                        published={published}
+                        tags={tags}
+                    />
+                    <button className={formStyle.deleteBtn} onClick={() => removePost(i)}>
+                        <Delete />
+                    </button>
+                </div>
+            ))}
         </>
     );
 }
